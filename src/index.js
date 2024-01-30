69a8613f73d5b6984b9f7910cbf4aa8192b7a54c
alex <sytnikov.zp@gmail.com>
2024-01-30 23:21:53 +0200

    Fix formula in loop

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 'use strict';
 
-const COUNTOFBALLS = 12;
+const COUNT_OF_BALLS = 12;
 const DIAMETER = 0.5;
 const THICKNESS = 0.01;
 let volumeOfSmallestBall = 0;
 let commonVolume = 0;
-let result = 0;
+let currentVolume = 0;
 
 volumeOfSmallestBall = (Math.PI * DIAMETER ** 3) / 6;
 
-for (let i = 1; i <= COUNTOFBALLS; i++) {
-  result = volumeOfSmallestBall + THICKNESS;
-  commonVolume += result;
-  console.log(`The volume of ${i} ball(s) with thickness is ${commonVolume} meters^3`)
+for (let i = 1; i <= COUNT_OF_BALLS; i++) {
+  currentVolume = Math.PI * (DIAMETER + THICKNESS) ** 3 / 6;
+  commonVolume += currentVolume;
 }
+
+console.log(`The volume of ${COUNT_OF_BALLS} ball(s) with thickness is ${commonVolume} meters^3`)
