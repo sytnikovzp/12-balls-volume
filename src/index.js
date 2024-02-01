420a4c71c02c0e43b449880e39476a6aa08c9f1f
alex <sytnikov.zp@gmail.com>
2024-02-01 20:13:34 +0200

    Fix formula to correct formula!!!

--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@
 const COUNT_OF_BALLS = 12;
 const DIAMETER = 0.5;
 const THICKNESS = 0.01;
-let volumeOfSmallestBall = 0;
 let commonVolume = 0;
-let currentVolume = 0;
-let currentDiameter = 0;
-
-volumeOfSmallestBall = (Math.PI * DIAMETER ** 3) / 6;
 
 for (let i = 1; i <= COUNT_OF_BALLS; i++) {
-  currentDiameter = DIAMETER + 2 * i * THICKNESS;
-  currentVolume = Math.PI * currentDiameter ** 3 / 6;
+  let currentDiameter = DIAMETER + 2 * (i-2) * THICKNESS;
+  let currentVolume = Math.PI * currentDiameter ** 3 / 6;
   commonVolume += currentVolume;
 }
 
